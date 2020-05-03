5d5ee9f6b2fe276de9a8d21903c041ace9cfa462
alumezi <arbnorlumezi@gmail.com>
2020-05-03 19:47:47 +0200

    1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,18 +3,10 @@ import ReactDOM from 'react-dom'
 
 const Info = ({ name, number }) => <p>{name} {number || 0}</p>;
 
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const allFeedback = good + neutral + bad;
-
+const Statistics = ({ stats }) => {
+  const { good, neutral, bad, allFeedback } = stats;
   return (
-    <div>
-      <h1>Give Feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+    <>
       <h2>Statistics</h2>
       <Info name={"good"} number={good} />
       <Info name={"neutral"} number={neutral} />
@@ -22,6 +14,25 @@ const App = () => {
       <Info name={"all"} number={allFeedback} />
       <Info name={"average"} number={(good - bad) / allFeedback} />
       <Info name={"positive"} number={(good / allFeedback) * 100} />
+    </>
+  )
+
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const allFeedback = good + neutral + bad;
+  console.log("App -> good", good)
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button> 
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Statistics stats={{ good, neutral, bad, allFeedback }} />
     </div>
   )
 }
