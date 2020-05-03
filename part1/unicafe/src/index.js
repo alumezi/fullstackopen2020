5c759ba5e3faea7cc6c5dbc2b1bd07bdbfdf2a5d
alumezi <arbnorlumezi@gmail.com>
2020-05-03 19:51:45 +0200

    1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -5,18 +5,23 @@ const Info = ({ name, number }) => <p>{name} {number || 0}</p>;
 
 const Statistics = ({ stats }) => {
   const { good, neutral, bad, allFeedback } = stats;
+
   return (
     <>
       <h2>Statistics</h2>
-      <Info name={"good"} number={good} />
-      <Info name={"neutral"} number={neutral} />
-      <Info name={"bad"} number={bad} />
-      <Info name={"all"} number={allFeedback} />
-      <Info name={"average"} number={(good - bad) / allFeedback} />
-      <Info name={"positive"} number={(good / allFeedback) * 100} />
+      {
+        allFeedback !== 0 ?
+          <>
+            <Info name={"good"} number={good} />
+            <Info name={"neutral"} number={neutral} />
+            <Info name={"bad"} number={bad} />
+            <Info name={"all"} number={allFeedback} />
+            <Info name={"average"} number={(good - bad) / allFeedback} />
+            <Info name={"positive"} number={(good / allFeedback) * 100} />
+          </>
+          : "No feedback given"}
     </>
   )
-
 }
 
 const App = () => {
@@ -29,7 +34,7 @@ const App = () => {
   return (
     <div>
       <h1>Give Feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button> 
+      <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
       <Statistics stats={{ good, neutral, bad, allFeedback }} />
