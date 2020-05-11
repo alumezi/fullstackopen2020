8ec86337509b7d3216bf0c9be7c85dc54feae9a8
alumezi <arbnorlumezi@gmail.com>
2020-05-11 14:54:45 +0200

    2.12*

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = (props) => {
+    const [countries, setCountries] = useState([{ name: "gemr" }, { name: "asdasd" }]);
+    const [query, setQuery] = useState('');
+    const [firstCountry, setFirstCountry] = useState({});
+
+    useEffect(() => {
+        (async () => {
+            try {
+                let response = await axios.get(`https://restcountries.eu/rest/v2/name/${query}`);
+                setCountries(response.data);
+                setFirstCountry(response.data[0]);
+            } catch (e) {
+                console.log(e.message);
+            }
+        })()
+    }, [query])
+
+    return (
+        <>
+            find countries<input type="search" name="countries" onChange={event => setQuery(event.target.value)} value={query} />
+            {countries.length > 10 ? <div>Too many matches, specify another filter</div> :
+                (countries.length > 1 ? countries.map(country => <div key={country.name}>{country.name}</div>) :
+                    <>
+                        <h1>{firstCountry.name}</h1>
+                        <p>Capital {firstCountry.capital}</p>
+                        <p>Population {firstCountry.population}</p>
+                        <h3>Languages</h3>
+                        <ul>
+                            {firstCountry.languages.map(lang => <li key={lang.name}>{lang.name}</li>)}
+                        </ul>
+                        <img src={firstCountry.flag} width="200" />
+                    </>
+                )}
+        </>
+    )
 }
-
-export default App;
